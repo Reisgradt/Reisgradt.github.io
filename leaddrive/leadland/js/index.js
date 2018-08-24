9d13adceae7153c58a06949cff8f1d4609f77c31
Deamilit <deamilit@gmail.com>
2018-08-24 19:03:50 +0300

    afwf

--- leaddrive/leadland/js/index.js
+++ leaddrive/leadland/js/index.js
@@ -1,3 +1,4 @@
+/* slider 1 */
 let slides = document.querySelectorAll('.slider__slide'),
     sliderRadio = document.querySelectorAll('.slider__radio__item');
 
@@ -16,3 +17,40 @@ function choseRadioBtn(e) {
 
     slides[e.target.dataset.number].classList.add('slider__slide_active');
 }
+
+/* slider 2 */
+let leftBtn = document.querySelector('.block6__btn_left'),
+    rightBtn = document.querySelector('.block6__btn_right'),
+    numSlider2 = document.querySelector('.block6__slider__number');
+
+let slides2 = document.querySelectorAll('.block6__slider__slide');
+
+let currentNum = 1;
+
+function changeSlide2(index) {
+    slides2.forEach(slide => {
+        slide.classList.remove('block6__slider__slide_active');
+    });
+    slides2[index].classList.add('block6__slider__slide_active');
+}
+
+function changeNumSlide2(num) {
+    let strNum = num.toString();
+    if (strNum.length === 1) strNum = '0' + strNum;
+    numSlider2.innerHTML = strNum;
+}
+
+function nextSlide2() {
+    if (currentNum + 1 > slides2.length) currentNum = 1;else currentNum++;
+    changeSlide2(currentNum - 1);
+    changeNumSlide2(currentNum);
+}
+
+function prevSlide2() {
+    if (currentNum - 1 < 1) currentNum = slides2.length;else currentNum--;
+    changeSlide2(currentNum - 1);
+    changeNumSlide2(currentNum);
+}
+
+leftBtn.addEventListener('click', prevSlide2);
+rightBtn.addEventListener('click', nextSlide2);
