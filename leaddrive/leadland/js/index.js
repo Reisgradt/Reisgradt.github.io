4ac71caf58cba06601ef154d439d5f3ccc753422
Deamilit <deamilit@gmail.com>
2018-08-28 18:57:26 +0300

    Animation

--- leaddrive/leadland/js/index.js
+++ leaddrive/leadland/js/index.js
@@ -75,4 +75,94 @@
         login.classList.toggle('login_active');
         nameField.focus();
     }
+
+    /* Анимации */
+    let os = new OnScreen({
+        tolerance: 0,
+        debounce: 0
+    });
+
+    function addCl(el, cl, time) {
+        setTimeout(() => el.classList.add(cl + '_anim'), time);
+    }
+
+    function anim(cl, time) {
+        os.on('enter', '.' + cl, el => addCl(el, cl, time));
+    }
+
+    function animChildren(cl, chcl, timeDelay) {
+        os.on('enter', '.' + cl, el => {
+            let i = 0;
+            Array.from(el.children).forEach(child => {
+                if (child.classList.contains(chcl)) {
+                    addCl(child, chcl, timeDelay * i);
+                    i++;
+                }
+            });
+        });
+    }
+
+    /* b1 title1 */
+    anim('block1__first__item', 100);
+
+    /* b1 slogan2 */
+    anim('block1__slogan2', 1000);
+
+    /* b1 btn */
+    anim('block1__btn', 1500);
+
+    animChildren('block2__table__content', 'block2__table__row', 140);
+
+    //anim('block3__title', 100);
+    animChildren('block3__middle', 'block3__work-with-item', 140);
+
+    animChildren('block3__work-with-systems', 'block3__work-with-systems__item', 200);
+
+    animChildren('block4__right-side', 'block4__item', 500);
+
+    anim('block4__icon', 100);
+    anim('block4__title', 100);
+    anim('block4__earning', 100);
+
+    animChildren('block5__img', 'block5__item', 500);
+
+    function toTopScreenOS(osMoney, el) {
+        osMoney.destroy();
+
+        let items = Array.from($('.block5__table__earnings'));
+
+        var i = items.length;
+
+        function toTopSpin(index) {
+            if (i < 0) {
+                return;
+            }
+
+            $(items[index]).spincrement({
+                thousandSeparator: ' ',
+                duration: 500,
+                fade: false,
+                complete: toTopSpin.bind(null, index - 1)
+            });
+        }
+        toTopSpin(i - 1);
+        $('.block5__table__earnings').css('opacity', .7);
+    }
+
+    let osMoney = new OnScreen({
+        tolerance: 0,
+        debounce: 0
+    });
+
+    osMoney.on('enter', '.block5__table__row:nth-child(7)', toTopScreenOS.bind(null, osMoney));
+
+    let osSlider2 = new OnScreen();
+
+    function slider2Activate(_osSlider2) {
+        _osSlider2.destroy();
+        console.log(1);
+        setInterval(nextSlide2, 6000);
+    }
+
+    osSlider2.on('enter', '.block6__slider', el => slider2Activate.bind(null, osSlider2));
 })();
