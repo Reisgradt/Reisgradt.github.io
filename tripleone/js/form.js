cd17b9e3c18fe0d4e8354b6436c01cae1f24e781
Deamilit <deamilit@gmail.com>
2018-10-09 01:30:02 +0300

    2222

--- tripleone/js/form.js
+++ tripleone/js/form.js
@@ -1,10 +1,13 @@
 (function () {
+    /* Flags */
     $("#phone").intlTelInput({
         allowDropdown: false
     });
 
+    /* Mask */
     $("#phone").mask("+9      999 999 99 99");
 
+    /* Timer */
     let timers = document.querySelectorAll('.form-remaining__timer');
 
     function changeTimerValue(currentValue) {
@@ -26,4 +29,72 @@
     }
 
     timers.forEach(item => timer(item));
+
+    /* Custom Select */
+
+    function getChild(el, childClass) {
+        for (let i = 0; i < el.children.length; ++i) {
+            if (el.children[i].classList.contains(childClass)) return el.children[i];
+        }
+    }
+
+    function getNeighbor(el, neighborClass) {
+        let parent = el.parentElement;
+        return getChild(el.parentElement, neighborClass);
+    }
+
+    function doSelect(selectEl, containerCl, choosenCl) {
+        /* Сам селект */
+        let select = getChild(selectEl, 'custom-select__select');
+
+        /* Контейнер для заполнения */
+        let optionContainer = getChild(selectEl, containerCl);
+        /* Текущий элемент выбора */
+        let choosenEl = getChild(selectEl, choosenCl);
+
+        /* Создать на каждый option pseudoOption */
+        Array.from(select.children).forEach(item => {
+            if (item.disabled) return;
+
+            let pseudoOption = document.createElement('div');
+            pseudoOption.classList.add('custom-select__item');
+            pseudoOption.innerHTML = item.innerHTML;
+            pseudoOption.dataset.value = item.value;
+
+            optionContainer.appendChild(pseudoOption);
+        });
+
+        /* Записать choosen элемент */
+        choosenEl.innerHTML = select.options[select.selectedIndex].innerHTML;
+
+        /* Открытие и закрытие контейнера выпадающего списка */
+        selectEl.addEventListener('click', e => {
+            optionContainer.classList.toggle(containerCl + '_active');
+            selectEl.classList.toggle('custom-select_active');
+        });
+
+        /* Сворачивание контейнера при нажатии вне его границ */
+        document.body.addEventListener('click', e => {
+            if (e.target === selectEl || e.target === optionContainer || e.target === choosenEl) return;
+            optionContainer.classList.remove(containerCl + '_active');
+            selectEl.classList.remove('custom-select_active');
+        });
+
+        /* При нажатии на элемент, выбрать его */
+        Array.from(optionContainer.children).forEach(item => {
+            item.addEventListener('click', selectItem);
+        });
+
+        function selectItem(e) {
+            Array.from(select.options).forEach(option => {
+                if (option.value == e.target.dataset.value) {
+                    option.selected = true;
+                }
+            });
+
+            choosenEl.innerHTML = select.options[select.selectedIndex].innerHTML;
+        }
+    }
+
+    doSelect(document.querySelector('.custom-select'), 'custom-select__container', 'custom-select__choosen');
 })();
