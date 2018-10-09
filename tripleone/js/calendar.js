939fe5519b03d2be30513ee81ee9733474c4cdcf
Deamilit <deamilit@gmail.com>
2018-10-10 00:18:47 +0300

    Sync calendar

--- tripleone/js/calendar.js
+++ tripleone/js/calendar.js
@@ -19,11 +19,21 @@
         event.target.classList.add('jsCalendar-current');
     });
 
+    calendar.onMonthChange(changeDate);
+
+    function changeDate() {
+        let calendarTitle = document.querySelector('.jsCalendar-title-name');
+        let contentTitle = document.querySelector('.current-month');
+        contentTitle.innerHTML = calendarTitle.innerHTML;
+    }
+
     document.querySelector('.last-month').addEventListener('click', () => {
         calendar.previous();
+        changeDate();
     });
 
     document.querySelector('.next-month').addEventListener('click', () => {
         calendar.next();
+        changeDate();
     });
 })();
